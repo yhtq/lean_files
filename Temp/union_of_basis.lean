b3c671e2f80ccbfb7cd46b8b346bb04dca681cd5
yhtq <1414672068@qq.com>
2024-12-03 13:01:58 +0800

    12.3

--- Temp/union_of_basis.lean
+++ Temp/union_of_basis.lean
@@ -67,10 +67,88 @@ theorem IsTranscendenceBasis.iff_isAlgebraic {R A I : Type*} [CommRing R] [CommR
         have := AlgebraicIndependent.transcendental_extends (x := s1) (s := Set.range s) hs1 hs2 (a := a) (by aesop)
         contradiction
 
-theorem IsAlgebraic.on_algebra_adjoin_iff_on_field_adjoin {R A : Type*} [Field R] [Field A] [Algebra R A] [Nontrivial R] {s : Set A} :
-  Algebra.IsAlgebraic (Algebra.adjoin R s) A ↔ Algebra.IsAlgebraic (IntermediateField.adjoin R s) A := by sorry
+instance algebra_adjoin {K L : Type*} [Field K] [Field L] [Algebra K L]  {s : Set L} :
+  Algebra (Algebra.adjoin K s) (IntermediateField.adjoin K s) := RingHom.toAlgebra (Subalgebra.inclusion (IntermediateField.algebra_adjoin_le_adjoin K s))
 
-#leansearch "mem Algebra.adjoint iff?"
+lemma adjoin_on_algebra_adjoin_eq_adjoin {K L : Type*} [Field K] [Field L] [Algebra K L]  {s : Set L} :
+  IntermediateField.adjoin K (Algebra.adjoin K s) = IntermediateField.adjoin K s := by
+    apply le_antisymm
+    · have := IntermediateField.adjoin.mono K _ _ (SetLike.coe_subset_coe.mpr (IntermediateField.algebra_adjoin_le_adjoin K s))
+      simp only [IntermediateField.coe_toSubalgebra, IntermediateField.adjoin_self] at this
+      exact this
+    · apply IntermediateField.adjoin.mono
+      exact Algebra.subset_adjoin
+
+lemma Subalgebra.inclusion_range_subtype_val {R A : Type*} [CommRing R] [CommRing A] [Algebra R A] {S T : Subalgebra R A} (h : S ≤ T) :
+  Subtype.val '' (Set.range (Subalgebra.inclusion h)) = S := by
+    aesop
+
+set_option synthInstance.maxHeartbeats 200000
+set_option maxHeartbeats 400000
+instance {K L : Type*} [Field K] [Field L] [Algebra K L]  {s : Set L} :
+  IsFractionRing (Algebra.adjoin K s) (IntermediateField.adjoin K s) := by
+    -- it's easy in math that K(s) is the fraction field of K[s]
+    let inclusion := Subalgebra.inclusion (IntermediateField.algebra_adjoin_le_adjoin K s)
+    let inclusion_injective : Function.Injective inclusion := Subalgebra.inclusion_injective (IntermediateField.algebra_adjoin_le_adjoin K s)
+    -- manually write these instances because time out
+    letI : Module (Algebra.adjoin K s) (IntermediateField.adjoin K s) := Algebra.toModule
+    letI : NoZeroSMulDivisors (Algebra.adjoin K s) (IntermediateField.adjoin K s) := NoZeroSMulDivisors.iff_algebraMap_injective.mpr inclusion_injective
+
+    letI := FractionRing.liftAlgebra (Algebra.adjoin K s) (IntermediateField.adjoin K s)
+
+    -- we use the fact that, the canolical lift map of inclusion can be proved to be bijective, hence the fraction field of K[s] is isomorphic to K(s)
+    let lift_inclusion : (FractionRing (Algebra.adjoin K s)) →ₐ[K] (IntermediateField.adjoin K s) := IsFractionRing.liftAlgHom inclusion_injective
+    let lift_inclusion_equiv := AlgEquiv.ofBijective lift_inclusion (
+      by
+        constructor
+        · unfold lift_inclusion IsFractionRing.liftAlgHom
+          simp only [IsLocalization.coe_liftAlgHom, AlgHom.toRingHom_eq_coe]
+          rw [IsLocalization.lift_injective_iff]
+          unfold inclusion
+          intro x y
+          constructor
+          intro a
+          simp_all only [IsFractionRing.coe_inj, RingHom.coe_coe]
+          intro h
+          -- don't know why rw [`Subalgebra.coe_toRingHom`] doesn't work
+          have : inclusion x = inclusion y := by
+            unfold inclusion
+            exact h
+          aesop
+        · rw [<-Set.range_eq_univ]
+          unfold lift_inclusion
+          rw [<-AlgHom.coe_fieldRange]
+          rw [IsFractionRing.liftAlgHom_fieldRange]
+          · unfold inclusion
+            simp only [AlgHom.coe_range]
+            rw [<-IntermediateField.coe_top (F := K)]
+            rw [SetLike.coe_set_eq]
+            apply_fun IntermediateField.lift (L := L) <;> simp only [IntermediateField.lift_adjoin,
+              IntermediateField.lift_top, IntermediateField.lift_injective]
+            simp_rw [<-adjoin_on_algebra_adjoin_eq_adjoin (s := s)]
+            apply congrArg
+            exact Subalgebra.inclusion_range_subtype_val _
+    )
+    rw [IsFractionRing]
+    let lift_inclusion_equiv' : (FractionRing (Algebra.adjoin K s)) ≃ₐ[(Algebra.adjoin K s)] (IntermediateField.adjoin K s) := AlgEquiv.ofRingEquiv (f := lift_inclusion_equiv) (by
+      intro x
+      aesop
+    )
+    have := IsLocalization.isLocalization_iff_of_algEquiv ((nonZeroDivisors (Algebra.adjoin K s)) ) lift_inclusion_equiv'.symm (R := (Algebra.adjoin K s))
+    rw [this]
+    exact Localization.isLocalization
+
+
+
+theorem IsAlgebraic.on_algebra_adjoin_iff_on_field_adjoin {R A : Type*} [Field R] [Field A] [Algebra R A] [Nontrivial R] {s : Set A} :
+  Algebra.IsAlgebraic (Algebra.adjoin R s) A ↔ Algebra.IsAlgebraic (IntermediateField.adjoin R s) A := by
+      haveI : IsScalarTower ((Algebra.adjoin R s)) ((IntermediateField.adjoin R s)) A
+       := IsScalarTower.of_algebraMap_smul fun r ↦ congrFun rfl
+      -- wu use the fact that if K is the fraction field of A, then C is algebraic on A iff C is algebraic on K
+      rw [IsFractionRing.comap_isAlgebraic_iff (K := (IntermediateField.adjoin R s))]
+
+
+
 theorem union_of_basis_is_basis
     {F K E : Type*}
     [Field F] [Field K] [Field E]
@@ -107,11 +185,7 @@ theorem union_of_basis_is_basis
     have (p : MvPolynomial I2 (MvPolynomial I1 F)) : (MvPolynomial.aeval (Sum.elim MvPolynomial.X (MvPolynomial.C ∘ s))) (MvPolynomial.iterToSum F I2 I1 p) =  MvPolynomial.map (MvPolynomial.aeval s (R := F)) p := by
       apply MvPolynomial.induction_on p
       · simp [MvPolynomial.aeval_rename]
-        intro a
-        rw [MvPolynomial.aeval_def]
-        apply congrFun
-        apply congrArg
-        rfl
+        aesop
       · intro p q hp hq
         -- strangely can't find the instance unless manually specify f
         repeat rw [map_add (f := MvPolynomial.iterToSum F I2 I1)]
@@ -129,19 +203,18 @@ theorem union_of_basis_is_basis
     · apply MvPolynomial.map_injective
       exact hs1
   ·
-    -- to show $E$ is algebraic over $F[s, t]$, just use the transitivity of algebraic extension and $K$ is algebraic over $F[t]$, hence $K[s]$ is algebraic over $F[t, s]$, hence $E$ is algebraic over $F[t, s]$.
+    -- pay attention that the definition of basis in mathlib is $K$ is algebraic on $F[t]$, rather than on $F(s)$ which the stack project uses, but they are equivalent.
+    -- to show $E$ is algebraic over $F(s, t)$, just use the transitivity of algebraic extension and $K$ is algebraic over $F(t)$, hence $K(s)$ is algebraic over $F(t, s)$, hence $E$ is algebraic over $F(t, s)$.
     apply IsTranscendenceBasis.isAlgebraic at hs
     apply IsTranscendenceBasis.isAlgebraic at ht
     rw [IsAlgebraic.on_algebra_adjoin_iff_on_field_adjoin] at *
     -- rw [Algebra.isAlgebraic_def]
     -- intro x
-    have : Set.range basis_union = (Set.range t) ∪ ((algebraMap K E) '' (Set.range s)) := sorry
+    have : Set.range basis_union = (Set.range t) ∪ ((algebraMap K E) '' (Set.range s)) := by
+      unfold basis_union
+      aesop
     rw [this]
     rw [<-IntermediateField.adjoin_adjoin_left, IntermediateField.adjoin_adjoin_comm]
     letI : Algebra (IntermediateField.adjoin F (Set.range basis_union)) (IntermediateField.adjoin K (Set.range t)) := by
       apply IntermediateField.algebra'
     apply Algebra.IsAlgebraic.trans (L := (IntermediateField.adjoin K (Set.range t)))
-
-
-
-#leansearch "extension of algebric extension is ?"
